7815648863a5e2ce629e506f715e83d1f22d903c
Edu <eduadrianovargasholguin@gmail.com>
2024-09-14 23:38:18 -0500

    Se agrego mantenedores de producto y stock

---
+++ FNT_VENTAS/Controllers/productoController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FNT_DataModel;
+
+namespace FNT_VENTAS.Controllers
+{
+    public class productoController : Controller
+    {
+        private DBSisVentasEntities db = new DBSisVentasEntities();
+
+        // GET: producto
+        public ActionResult Index()
+        {
+            var producto = db.producto.Include(p => p.categoria);
+            return View(producto.ToList());
+        }
+
+        // GET: producto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            producto producto = db.producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: producto/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCategoria = new SelectList(db.categoria, "idCategoria", "descripcion");
+            return View();
+        }
+
+        // POST: producto/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idProducto,productoDescripcion,precioUnitario,idCategoria,usuario_creacion,fecha_creacion,usuario_modificacion,fecha_modificacion")] producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.producto.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCategoria = new SelectList(db.categoria, "idCategoria", "descripcion", producto.idCategoria);
+            return View(producto);
+        }
+
+        // GET: producto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            producto producto = db.producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCategoria = new SelectList(db.categoria, "idCategoria", "descripcion", producto.idCategoria);
+            return View(producto);
+        }
+
+        // POST: producto/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idProducto,productoDescripcion,precioUnitario,idCategoria,usuario_creacion,fecha_creacion,usuario_modificacion,fecha_modificacion")] producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCategoria = new SelectList(db.categoria, "idCategoria", "descripcion", producto.idCategoria);
+            return View(producto);
+        }
+
+        // GET: producto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            producto producto = db.producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            producto producto = db.producto.Find(id);
+            db.producto.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FNT_VENTAS/Controllers/stockController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FNT_DataModel;
+
+namespace FNT_VENTAS.Controllers
+{
+    public class stockController : Controller
+    {
+        private DBSisVentasEntities db = new DBSisVentasEntities();
+
+        // GET: stock
+        public ActionResult Index()
+        {
+            var stock = db.stock.Include(s => s.producto);
+            return View(stock.ToList());
+        }
+
+        // GET: stock/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            stock stock = db.stock.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // GET: stock/Create
+        public ActionResult Create()
+        {
+            ViewBag.idProducto = new SelectList(db.producto, "idProducto", "productoDescripcion");
+            return View();
+        }
+
+        // POST: stock/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idStock,idProducto,stockActual,stockMinimo,stockMaximo,usuario_creacion,fecha_creacion,usuario_modificacion,fecha_modificacion")] stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                db.stock.Add(stock);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idProducto = new SelectList(db.producto, "idProducto", "productoDescripcion", stock.idProducto);
+            return View(stock);
+        }
+
+        // GET: stock/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            stock stock = db.stock.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idProducto = new SelectList(db.producto, "idProducto", "productoDescripcion", stock.idProducto);
+            return View(stock);
+        }
+
+        // POST: stock/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idStock,idProducto,stockActual,stockMinimo,stockMaximo,usuario_creacion,fecha_creacion,usuario_modificacion,fecha_modificacion")] stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stock).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idProducto = new SelectList(db.producto, "idProducto", "productoDescripcion", stock.idProducto);
+            return View(stock);
+        }
+
+        // GET: stock/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            stock stock = db.stock.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // POST: stock/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            stock stock = db.stock.Find(id);
+            db.stock.Remove(stock);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
